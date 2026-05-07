0dbbe98e6b207a0a2a4720baffa74acba46c5531
PhoenixProdigy05 <Ivanboye592@outlook.com>
2026-05-07 14:26:42 +0000

    fix: Hardcode API URL in next.config.ts

--- System/Frontend/next.config.ts
+++ System/Frontend/next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: 'https://makerspace-system-backend.onrender.com/api',
+  },
+};
 
 export default nextConfig;
